6c54ac5d8709903ac9b816d3a1255e8462795815
Yo Akiyama <yoakiyama16@gmail.com>
2024-04-08 22:30:34 -0400

    data directory, files for publishing page, and fp2 route directory

---
+++ src/routes/fp2/mapbox.svelte
@@ -0,0 +1,14 @@
+<script>
+    import mapboxgl from 'mapbox-gl';
+    const accessToken = 'pk.eyJ1IjoiZXB0eXNpbmdlciIsImEiOiJjbHVoanlneWIycm14MmxvZTh2Y3VhYXFkIn0.lkhHKBe-C2_I9v2J-jJ2hg'; // Replace with your token
+  
+    onMount(() => {
+      mapboxgl.accessToken = accessToken;
+      map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/examples/cjfioozof002u2sr5k7t14dim', // Replace with your desired map style
+        center: [-74.50, 40.7], // Replace with your initial center coordinates
+        zoom: 9 // Set the initial zoom level
+      });
+    });
+  </script>
---
+++ src/routes/fp2/rent_slider.svelte
@@ -0,0 +1,36 @@
+<script>
+	// Parameters for the slider
+	export let rentValue = 1500;
+    export let maxRent = 2000;
+    export let minRent = 0;
+
+</script>
+
+<div class="slider-container">
+	<label for="rent-slider">Maximum spending on rent: </label>
+	<input type=range bind:value={rentValue} min={minRent} max={maxRent} name='rent-slider'>
+	<span>{ rentValue }</span>
+</div>
+
+<style>
+	.slider-container {
+		position: fixed;
+		bottom: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		width: 20%;
+		background-color: white;
+		padding: 20px;
+		border-radius: 10px;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+	}
+	input[type="range"]{
+		width: 100%;
+	}
+	span {
+		display: inline-block;
+		padding: 0 .4em;
+		border-radius: .3em;
+		background: oklch(95% 2% 200);
+	}
+</style>
