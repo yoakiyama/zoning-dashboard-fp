f96a2e245de36d67ad2ffcd240199e815013ff67
Megan Le <meganlecs@gmail.com>
2024-04-28 17:48:11 -0400

    resources page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -8,16 +8,21 @@
 <script>
     import DashboardTab from "./Dashboard.svelte"
     import Methodology from "./Methodology.svelte"
+    import Resources from "./Resources.svelte"
     import Tabs from "./Tabs.svelte";
 
       let items = [
     { label: "Dashboard",
 		 value: 1,
 		 component: DashboardTab
-		},
+	},
     { label: "Methodology",
         value: 2,
         component: Methodology
+    },
+    { label: "What's next?",
+        value: 3,
+        component: Resources
     }
   ];
 </script>
@@ -28,49 +33,4 @@
 
 <style>
     @import url("$lib/global.css");
-    .closeButton:hover,
-    .closeButton:focus {
-        font-weight: bold;
-        color: #d9534f;
-        outline: none;
-    }
-
-    .popUp {
-        position: absolute;   /* Absolute positioning relative to the nearest positioned ancestor */
-        bottom: 0;            /* Aligns the container to the bottom */
-        left: 0;              /* Aligns the container to the left */
-        margin: 20px;         /* Adds some space from the corner edges */
-        padding: 5px;        /* Padding inside the container */
-        z-index: 1000;
-        background-color: rgba(255, 255, 255, 0.9); /* Semi-transparent white background */
-        border-radius: 8px;   /* Rounded corners for aesthetics */
-        font-size: 16px;      /* Adequate font size for visibility */
-        box-shadow: 0 2px 4px rgba(0,0,0,0.2); /* Subtle shadow for better readability */
-        max-width: 400px;     /* Maximum width to avoid overly wide text block */
-    }
-    .checkboxContainer {
-        position: relative; /* or 'absolute' if needed */
-        z-index: 1000; /* higher than the map's z-index */
-    }
-
-    input[type='checkbox'] {
-        accent-color: hsl(200, 50%, 50%);
-    }
-
-    .wrapper {
-        position: absolute;
-        margin: 20px;
-        width: 300px;
-        z-index: 1000;
-        text-align: left;
-        margin-top: 15px;
-        font-size: 16px;
-    }
-
-    .checkboxContainer, .dropdownContainer {
-        margin-bottom: 5px; /* Adds space between the checkbox and dropdown */
-        width: 100%; /* Ensures children stretch to match the wrapper's width */
-    }
-
-
 </style>
---
+++ src/routes/dashboard/Resources.svelte
@@ -0,0 +1,24 @@
+<div class="text-container">
+
+<h2>Learn More</h2>
+<p>Today, more than half of the residents in Boston are considered rent burdened<sup><a href="https://homesforprofit.mapc.org/report">1</a></sup>, raising the question of what can be done to improve housing affordability and accessibility to all parts of Boston. You can learn more about proposed legislation and housing policies, as well as ways to get involved, by exploring the resources below.</p>
+
+<p><a href="https://www.mapc.org/get-involved/legislative-priorities/#housing">MAPC Legislative Priorities</a>: the Metropolitan Area Planning Commission's housing legislative agenda that includes initiatives and recommenations for updated approaches to housing construction, zoning, subsidies, transportation, and renter protections</p>
+
+<p><a href="https://www.mass.gov/info-details/multi-family-zoning-requirement-for-mbta-communities">MBTA Communities Act</a>: legislation that mandates upzoning around T or Commuter Rail stations</p>
+
+<p><a href="https://www.mass.gov/news/healey-driscoll-administration-unveils-4-billion-affordable-homes-act-to-increase-production-and-lower-costs">Affordable Homes Act</a>: a state-wide bill that will create over 40,000 new homes, as well as impose transfer fees on large real estate sales to deter housing speculation</p>
+
+<p><a href="https://www.progressivemass.com/issues/transfer-fee-2023-action/#about">Local Option Transfer Fees bill</a>: proposed state-wide bills that impose transfer fees on large real estate sales to deter housing speculation</p>
+
+<p><a href="https://www.topa4ma.org/">Tenant Opportunity to Purchase Act (TOPA)</a>: proposed state-wide bills that gives tenants the right to purchase property instead of being displaced upon foreclosures or short sales</p>
+
+</div>
+<style>
+    @import url("$lib/global.css");
+    .text-container {
+        margin: auto;
+        width: 100%;
+        line-height: 2;
+    }
+</style>
