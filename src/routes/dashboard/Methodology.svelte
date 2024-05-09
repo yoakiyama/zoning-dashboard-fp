77432e41b9c70b4b6edfae14620532a9edd099ff
Yo Akiyama <yoakiyama16@gmail.com>
2024-05-09 13:10:30 -0400

    Update methods section with rent data description

--- src/routes/dashboard/Methodology.svelte
+++ src/routes/dashboard/Methodology.svelte
@@ -1,10 +1,11 @@
 <div class="text-container">
 
-<p>The code we used for data processing and transformation is available on <a href="https://github.com/yoakiyama/zoning-dashboard-fp">GitHub</a>. This project was developed with guidance and feedback from the <a href="https://www.mapc.org/">Metropolitan Area Planning Commission (MAPC)</a>.</p>
+<p>The code we used for data processing and transformation is available on <a href="https://github.com/yoakiyama/zoning-dashboard-fp">GitHub</a>. This project was developed with guidance and feedback from the <a href="https://www.mapc.org/">Metropolitan Area Planning Commission (MAPC)</a> as well as Professors Catherine D'Ignazio and Arvind Satyanarayan.</p>
 
 <h2>Rent Data</h2>
-<p>We used rental data from <a href="https://www.zillow.com/research/data/">Zillow</a> to calculate average rent by neighborhood. We divided the rent by number of bedrooms when comparing values across neighborhoods. We also tracked average price by unit type (i.e., number of bedrooms) per neighborhood.</p>
-
+<p>The rent price data represents the <a href="https://www.bostonhousing.org/BHA/media/Documents/Leased%20Housing/SAFMRs/2024-Payment-Standards-All-BR.pdf">Boston Housing Authority's (BHA) 2024 Payment Standards</a> across neighborhoods. These prices establish the maximum BHA subsidy for a unit, which are they are based on  <a href="https://www.huduser.gov/portal/datasets/fmr.html">U.S. Department of Housing and Urban Development (HUD) Metropolitan Area Fair Market Rents</a> 
+    and <a href="https://www.huduser.gov/portal/datasets/fmr.html">HUD Small Area Fair Market Rents</a>. These rents represent the 40th percentile rents in specific neighborhoods.</p>
+    
 <h2>Commute Time Data</h2>
 <p>To calculate representative commute times between neighborhoods, we first calculated average travel times between MBTA stations from the <a href="https://mbta-massdot.opendata.arcgis.com/datasets/b08854b18ad942e4bc471c8c20e80792/about">MBTA ridership data</a>. For each neighborhood, we then computed []. In our initial exploratory data analysis, we also analyzed responses from the 2017-2021 <a href="https://www.bostonplans.org/getattachment/e8629081-b656-4c43-b38a-b0cb5e7e3b4b">American Community Survey</a> by looking at the relationship between commute times and other factors such as rent.</p>
 
