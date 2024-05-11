86bc0670e9e199ab8568429dcad8cd816f5cc144
Yo Akiyama <yoakiyama16@gmail.com>
2024-05-11 17:08:04 -0400

    Add section on mental health

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,15 +69,33 @@
 </script>
 
 <div class="intro-container">
-    <h2>How are transportation options and rent prices limiting your access to high-paying jobs?</h2>
-    <p>As rent prices climb in the Boston area, many tenants are being driven farther away from the center of the city<sup><a href="https://www.cbsnews.com/boston/news/boston-high-rent-city-workers-city-council-residence-requirement/">1</a>, <a href="https://www.forbes.com/sites/andrewdepietro/2023/01/24/the-average-rent-in-boston-now-rivals-bay-area-cities/?sh=7e5080d9434b">2</a></sup>. Unfortunately, this displacement often means living farther away from areas that are dense with high-paying job opportunities. Though many workers resort to long daily commutes, studies have also found that increased commute times are associated with reduced work and life satisfaction and decreased physical health<sup><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9819363/">3</a></sup>.</p>
+    <h2>How transportation options and rent prices are limiting your access to high-paying jobs</h2>
+    <p>As rent prices climb in the Boston area, many tenants are being driven farther away from the center of the 
+        city<sup><a href="https://www.cbsnews.com/boston/news/boston-high-rent-city-workers-city-council-residence-requirement/">1</a>, <a href="https://www.forbes.com/sites/andrewdepietro/2023/01/24/the-average-rent-in-boston-now-rivals-bay-area-cities/?sh=7e5080d9434b">2</a></sup>. 
+        Unfortunately, this displacement often means living farther away from areas that are dense with high-paying job opportunities.
+    </p>
+    <h3>Effects on mental and physical health</h3>
+    <p>
+        The trade off between affordability and commute time carries dire implications to overall health. According to the Metropolitan Area Planning Council (MAPC), 
+        a record <b>51% of Greater Boston renters are considered rent burdened</b>, spending more than the recommended maximum 30% of their monthly income on housing. 
+        Several longitudinal studies have documented how stress related to housing affordability and housing cost burden is significantly associated with 
+        declines in mental health and emotional well-being<sup><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8435361/">3</a></sup>. 
+        While many resort to long daily commutes, studies have also found that increased commute times are associated with reduced work 
+        and life satisfaction and decreased physical health<sup><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9819363/">4</a></sup>.
+    </p>
 
-    <p>But just how large is the trade-off between rent and commute time? Our analyses find that in the Boston area, a <b>$100 increase in rent</b> per bedroom is about equivalent to a <b>1 minute and 45 second decrease in commute</b> time. We also find complementary evidence that neighborhoods with high rent prices tend to have more businesses and job opportunities (normalized by population) and offer higher salaries. The following plot shows the average salary for workers in a given neighborhood regressed against the average 1 bedroom apartment rent (with data for both Boston and Cambridge area neighborhoods).
+    <p>
+        But just how large is the trade-off between rent and commute time? Our analyses find that in the Boston area, 
+        a <b>$100 increase in rent</b> per bedroom is about equivalent to a <b>1 minute and 45 second decrease in commute</b> time. 
+        We also find complementary evidence that neighborhoods with high rent prices tend to have more businesses and job opportunities 
+        (normalized by population) and offer higher salaries. The following plot shows the average salary for workers in a given neighborhood 
+        regressed against the average 1 bedroom apartment rent (with data for both Boston and Cambridge area neighborhoods).
     </p>
 
     <div id="salaryPlot"></div>
 
     <p>In the following dashboard, you’ll get to explore how the variables of rent, commute time, and salary interact with each other by seeing how high rent prices can reduce your housing options, then visualizing how existing transportation options can limit the areas you’re able to work in (depending on how long you would be willing to commute).</p>
+
     <button on:click={goToDashboard}>Enter</button>
 </div>
 
