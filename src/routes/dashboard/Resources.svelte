9c0f62d181b1455f89bef4d35db7b47667bf0643
Yo Akiyama <yoakiyama16@gmail.com>
2024-05-12 23:49:20 -0400

    Add information to the whats next tab

--- src/routes/dashboard/Resources.svelte
+++ src/routes/dashboard/Resources.svelte
@@ -1,13 +1,61 @@
 <div class="text-container">
 
-<h2>Learn More</h2>
-<p>Today, more than half of the residents in Boston are considered rent burdened<sup><a href="https://homesforprofit.mapc.org/report">1</a></sup>, raising the question of what can be done to improve housing affordability and accessibility to all parts of Boston. You can learn more about proposed legislation and housing policies, as well as ways to get involved, by exploring the resources below.</p>
+<h2>Learn about how you can get involved</h2>
+<p>
+    Through this interactive dashboard, we hope you were able to explore the difficult trade-offs that Bostonians must consider when 
+    finding housing that accomodates with their budget and commute time preferences. Given the ever increasing cost of rent and slow 
+    development of public transportation, urban planners, politicians, and voters in our communities must come together to improve housing affordability and 
+    public transportation in across the Boston area. Below, you can find a list of proposed 
+    legislation and housing policies. In these links, you will also find ways to get involved.
+</p>
 
-<p><a href="https://www.mapc.org/get-involved/legislative-priorities/#housing">MAPC Legislative Priorities</a>: the Metropolitan Area Planning Commission's housing legislative agenda that includes initiatives and recommenations for updated approaches to housing construction, zoning, subsidies, transportation, and renter protections</p>
+<p><a href="https://www.mapc.org/get-involved/legislative-priorities/#housing">MAPC Legislative Priorities</a>:</p>
+<ul>
+    <li>The Metropolitan Area Planning Commission's (MAPC) housing legislative agenda includes initiatives and recommenations for updates to 
+        housing construction, zoning, subsidies, transportation, and renter protections.</li>
+    <li>Here, you can find important information about the MAPC's Fiscal and Budget Priorities, which include the Massachusetts Rental Voucher Program (MRVP) 
+        and Massachusetts Bay Transportation Authority (MBTA). The MRVP provides rent vouchers, which can be an important resource for highly 
+        cost-burdened households (those who are paying over 50% of their income in rent) and homeless families. The MBTA is systematically underfunded, 
+        which has led to widespread safety concerns as well as common and substantial delays. The MAPC will focus on advocating for additional funding for the MBTA 
+        to address repairs and ensure regular service.
+    </li>
+</ul>
 
-<p><a href="https://www.mass.gov/info-details/multi-family-zoning-requirement-for-mbta-communities">MBTA Communities Act</a>: legislation that mandates upzoning around T or Commuter Rail stations</p>
+<p><a href="https://www.mbta.com/projects">Massachusetts Bay Transportation Authority (MBTA) projects page</a>:</p>
+<ul>
+    <li>Here, you can find all ongoing, recently completed, and contract awarded MBTA projects.</li>
+    <li>You can filter by transportation type (subway, bus, rail, or ferry), as well as the specific line for the subway in order to 
+        find information relevant to your commute.</li>
+    <li>Every project page details its goals, budget, status, and projected completion date.</li>
+    <li>You can also read more about the MBTA's Capital Investment Plan for fiscal year 2025 to 2029. This plan is updated annually in coordination with 
+        MassDOT and the <a href="https://www.ctps.org/">Boston Region Metropolitan Planning Organization</a>. To get involved, check out the 
+        <a href="https://www.ctps.org/calendar/month">MPO's meeting calendar</a>.</li>
+</ul>
 
-<p><a href="https://www.mass.gov/news/healey-driscoll-administration-unveils-4-billion-affordable-homes-act-to-increase-production-and-lower-costs">Affordable Homes Act</a>: a state-wide bill that will create over 40,000 new homes, as well as impose transfer fees on large real estate sales to deter housing speculation</p>
+<p><a href="https://www.mass.gov/info-details/multi-family-zoning-requirement-for-mbta-communities">MBTA Communities Act</a>: </p>
+<ul>
+    <li>The MBTA Communities Act was adopted in January 2021, and requires 177 Cities and Towns to establish at least 1 district within a 
+        half mile of public transportation (commuter rail, bus station, ferry terminal or subway) in which multi-family housing is permitted.
+    </li>
+    <li>The act also provides technical assistence via grant money to municipal planners. 
+        Since August 2022, over $6 million in technical assistance has been provided to 156 municipalities.</li>
+    <li>By upzoning around public transportation stations, the MBTA Communities Act should provide greater housing supply to these regions and increase accessibility to 
+        transportation.</li>
+</ul>
+
+
+<p><a href="https://www.mass.gov/news/healey-driscoll-administration-unveils-4-billion-affordable-homes-act-to-increase-production-and-lower-costs">Affordable Homes Act</a>:</p>
+<ul>
+    <li>Affordable Homes Act includes $4 billion in capital spending authorizations and 28 policy changes to address the 
+        critical housing supply shortage.</li>
+    <li>Crucially, it will fund the creation of more than 40,000 homes, including more than 22,000 new homes for low-income households 
+        and more than 12,000 homes for middle-income households.</li>
+    <li>It will also rehabilitate or make improvements improvements to 12,000 homes for low-income households, provide support for more than 11,000 moderate income 
+        households, and fund accessibility improvements for approximately 4,500 homes.</li>
+    <li>By increasing housing supply, this multi-billion dollar initiative may provide renters with more affordable options, alleviating the trade-off between rent cost 
+        and commute time.</li>
+    <li>As stated in the website, you can <a href="https://malegislature.gov/Search/FindMyLegislator">contact your legislative representative</a> to voice your support</li>
+</ul>
 
 <p><a href="https://www.progressivemass.com/issues/transfer-fee-2023-action/#about">Local Option Transfer Fees bill</a>: proposed state-wide bills that impose transfer fees on large real estate sales to deter housing speculation</p>
 
